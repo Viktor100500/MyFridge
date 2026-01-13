5c021f373e0e75a13a688cb08dc712ff67147911
Viktor Vlasov <vlasov.vik@gmail.com>
2026-01-13 20:28:33 +0100

    Initial Commit

---
+++ MyFridge/MyFridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFridgeApp.swift
+//  MyFridge
+//
+//  Created by Developer on 13.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MyFridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
